20bf8045e3a9cc2f303515c4e1a7d29dd2f63fa7
Thomas Jacques <jacquestom99@gmail.com>
2022-03-15 16:07:55 -0700

    added process.env var

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.get("/mynews/api/news", async (req, res) => {
   let mainChoice = req.query.mainchoice;
   let secondaryChoice = req.query.secondarychoice;
   let date = req.query.date;
-  let apikey = req.query.apiKey;
+  //let apikey = req.query.apiKey;
   let url = "";
 
   if (mainChoice === "everything") {
-    url = `https://newsapi.org/v2/everything?q=${secondaryChoice}&sortBy=relevency&language=en&from=${date}&apiKey=${apikey}`; // process.env.REACT_APP_NEWS_API_KEY2
+    url = `https://newsapi.org/v2/everything?q=${secondaryChoice}&sortBy=relevency&language=en&from=${date}&apiKey=${process.env.REACT_APP_NEWS_API_KEY2}`; // process.env.REACT_APP_NEWS_API_KEY2
   } else {
-    url = `https://newsapi.org/v2/top-headlines?language=en&from=${date}&apiKey=${apikey}`; // process.env.REACT_APP_NEWS_API_KEY2
+    url = `https://newsapi.org/v2/top-headlines?language=en&from=${date}&apiKey=${process.env.REACT_APP_NEWS_API_KEY2}`; // process.env.REACT_APP_NEWS_API_KEY2
   }
 
   request(url, function (error, response, body) {
