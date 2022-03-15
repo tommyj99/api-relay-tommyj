9a843c264134a4128fd369faf98555e184525ff6
Thomas Jacques <jacquestom99@gmail.com>
2022-03-15 16:31:31 -0700

    added apikey query

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.get("/mynews/api/news", async (req, res) => {
   let mainChoice = req.query.mainchoice;
   let secondaryChoice = req.query.secondarychoice;
   let date = req.query.date;
-  //let apikey = req.query.apiKey;
+  let apikey = req.query.apiKey;
   let url = "";
 
   if (mainChoice === "everything") {
-    url = `https://newsapi.org/v2/everything?q=${secondaryChoice}&sortBy=relevency&language=en&from=${date}&apiKey=${process.env.REACT_APP_NEWS_API_KEY2}`; // process.env.REACT_APP_NEWS_API_KEY2
+    url = `https://newsapi.org/v2/everything?q=${secondaryChoice}&sortBy=relevency&language=en&from=${date}&apiKey=${apikey}`; // process.env.REACT_APP_NEWS_API_KEY2
   } else {
-    url = `https://newsapi.org/v2/top-headlines?language=en&from=${date}&apiKey=${process.env.REACT_APP_NEWS_API_KEY2}`; // process.env.REACT_APP_NEWS_API_KEY2
+    url = `https://newsapi.org/v2/top-headlines?language=en&from=${date}&apiKey=${apikey}`; // process.env.REACT_APP_NEWS_API_KEY2
   }
 
   request(url, function (error, response, body) {
@@ -46,7 +46,9 @@ app.get("/mynews/api/news", async (req, res) => {
 // Any console.log statements you use in node for debugging will show up in your
 // terminal, not in the browser console!
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`my api connector app listening on port ${port}!`)
+);
 
 // let allowCrossDomain = function(req, res, next) {
 //   res.header('Access-Control-Allow-Origin', "*");
